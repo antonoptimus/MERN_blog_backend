bc8ff807b9afe846dc6d4c2604f05c24368666a0
Anton Eliseev <anton_saintp@mail.ru>
2023-11-10 18:19:09 +0300

    error handling and hiding the user's password from the response

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import UserModel from "./models/User.js";
 
 mongoose
   .connect(
-    "mongodb+srv://admin:wwwwww@cluster0.qxwbdst.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://admin:wwwwww@cluster0.qxwbdst.mongodb.net/blog?retryWrites=true&w=majority"
   )
   .then(() => console.log("DB ok"))
   .catch((error) => console.log("DB error", error));
@@ -18,27 +18,44 @@ const app = express();
 app.use(express.json());
 
 app.post("/auth/register", registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось зарегистрироваться",
+    });
   }
-
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
-
-  const user = await doc.save();
-
-  res.json({
-    success: true,
-  });
 });
 
 app.listen(4444, (err) => {
