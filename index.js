ac8ecdb1ac13f12304c67633da48671dbcc79f02
Anton Eliseev <anton_saintp@mail.ru>
2023-11-12 15:22:28 +0300

    file upload

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
-import { registerValidation, loginValidation, postCreateValidation } from "./validations.js";
+import multer from "multer";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
 import checkAuth from "./utils/checkAuth.js";
 
 import * as UserController from "./controllers/UserController.js";
@@ -15,19 +20,35 @@ mongoose
 
 const app = express();
 
-app.use(express.json());
+const storage = multer.diskStorage({
+  destination: (_, __, callback) => {
+    callback(null, "uploads");
+  },
+  filename: (_, file, callback) => {
+    callback(null, file.originalname);
+  },
+});
 
-app.post('/auth/login', loginValidation, UserController.login);
-app.post('/auth/register', registerValidation, UserController.register);
-app.get('/auth/me', checkAuth, UserController.getMe);
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use("/uploads/", express.static("uploads"));
+
+app.post("/auth/login", loginValidation, UserController.login);
+app.post("/auth/register", registerValidation, UserController.register);
+app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
 app.post("/posts", checkAuth, postCreateValidation, PostController.create);
 app.delete("/posts/:id", checkAuth, PostController.remove);
-app.patch("/posts/:id", checkAuth, PostController.update);
-
-
+app.patch("/posts/:id", checkAuth, postCreateValidation, PostController.update);
 
 app.listen(4444, (err) => {
   if (err) {
