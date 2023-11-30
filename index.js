50e885425c7301da8ca663c14a0ccf6dc81bc686
Anton Eliseev <anton_saintp@mail.ru>
2023-11-30 17:28:37 +0300

    deployment

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ import { handleValidationErrors, checkAuth } from "./utils/index.js";
 import { UserController, PostController } from "./controllers/index.js";
 
 mongoose
-  .connect(
-    "mongodb+srv://admin:wwwwww@cluster0.qxwbdst.mongodb.net/blog?retryWrites=true&w=majority"
-  )
+  .connect("procces.env.MONGODB_URI")
   .then(() => console.log("DB ok"))
   .catch((err) => console.log("DB error", err));
 
